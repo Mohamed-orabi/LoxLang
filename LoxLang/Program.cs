d2af64818f18b908da29f863e7717fda5bf282c7
Mohamed Orabi <moh.orabi97@gmail.com>
2024-10-25 16:47:40 +0300

    Add project files.

---
+++ LoxLang/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace LoxLang
+{
+    internal class Program
+    {
+        static bool hadError = false;
+
+        static void Main(string[] args)
+        {
+            string Path = @"D:\Code.txt";
+            RunFile(Path);
+        }
+
+        private static void RunFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            run(Encoding.Default.GetString(bytes));
+            if (hadError) Environment.Exit(65);
+        }
+
+        private static void run(String source)
+        {
+            //Scanner scanner = new Scanner(source);
+            //List<Token> tokens = scanner.scanTokens();
+            //// For now, just print the tokens.
+            //for (Token token : tokens)
+            //{
+            //    System.out.println(token);
+            //}
+        }
+
+        static void error(int line, String message)
+        {
+            report(line, "", message);
+        }
+        private static void report(int line, String where,
+        String message)
+        {
+            Console.WriteLine(
+            "[line " + line + "] Error" + where + ": " + message);
+            hadError = true;
+        }
+    }
+}
---
+++ LoxLang/Token.cs
@@ -0,0 +1,24 @@
+﻿namespace LoxLang
+{
+    public class Token
+    {
+        private readonly TokenType type;
+        private readonly string lexeme;
+        private readonly object literal;
+        private readonly int line;
+
+        public Token(TokenType type, string lexeme, object literal, int line)
+        {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+
+        public override string ToString()
+        {
+            return type + " " + lexeme + " " + literal;
+        }
+
+    }
+}
