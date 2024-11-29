be9cdad9c05678193dd3620f8eb49ea093f2f65d
Mohamed Orabi <moh.orabi97@gmail.com>
2024-11-29 17:30:33 +0200

    Creating Instances

--- LoxLang/LoxClass.cs
+++ LoxLang/LoxClass.cs
@@ -1,14 +1,25 @@
-﻿namespace LoxLang
+﻿
+namespace LoxLang
 {
-    public class LoxClass
+    public class LoxClass : LoxCallable
     {
-        private readonly string _name;
+        public readonly string _name;
 
         public LoxClass(string name)
         {
             _name = name;
         }
 
+        public object call(Interpreter interpreter, List<object> arguments)
+        {
+            LoxInstance instance = new LoxInstance(this);
+            return instance;
+        }
+
+        public int arity()
+        {
+            return 0;
+        }
         public override string ToString()
         {
             return _name;
---
+++ LoxLang/LoxInstance.cs
@@ -0,0 +1,19 @@
+﻿using static LoxLang.Stmt;
+
+namespace LoxLang
+{
+    public class LoxInstance
+    {
+        private readonly LoxClass _klass;
+
+        public LoxInstance(LoxClass klass)
+        {
+            _klass = klass;
+        }
+
+        public override string ToString()
+        {
+            return _klass._name + " instance";
+        }
+    }
+}
